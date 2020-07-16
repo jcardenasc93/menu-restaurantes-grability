41afa524630cb93ad1e6b0658a28beb50094880f
jcardenasc93 <jcardenasc93@gmail.com>
2020-07-16 00:17:43 -0500

    Crea prueba para validar que el endpoint '/restaurantes/' exista. (Falla)

--- restaurants/tests.py
+++ restaurants/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+import json
 
+from .models import Restaurant
 # Create your tests here.
+
+class RestaurantsTestCase(TestCase):
+    def setUp(self):
+        restaurant_1 = Restaurant.objects.create(name='Coma y vuelva')
+        restaurant_2 = Restaurant.objects.create(name='Restaurante Italiano')
+    
+    def test_ListRestaurants(self):
+        response = self.client.get('/restaurants/')
+        self.assertEqual(response.status_code, 200)
+
