9c303c21273213fe3734a5d0d474f67b8e9beead
jcardenasc93 <jcardenasc93@gmail.com>
2020-07-16 12:23:42 -0500

    Test case para validar el rechazo de las peticiones que no proporcionan un API-KEY valido (Falla)

--- restaurants/tests.py
+++ restaurants/tests.py
@@ -3,8 +3,8 @@ from django.urls import reverse
 import json
 from decimal import Decimal
 from .models import Restaurant, Product
-# Create your tests here.
 
+# Create your tests here.
 
 class RestaurantsTestCase(TestCase):
     # En esta clase serán ejecutadas las pruebas unitarias para endpoint que retorna el listado de restaurantes
@@ -23,7 +23,7 @@ class RestaurantsTestCase(TestCase):
         response = self.client.get(reverse('list_restaurants'), formal='json')
         response_data = json.loads(response.content)
         restaurant_1 = Restaurant.objects.get(name='Coma y vuelva')
-
+        
         for resturant in response_data['restaurants']:
             # Busca el restaurante que coincida con el nombre definido
             if resturant['name'] == 'Coma y vuelva':
@@ -32,6 +32,13 @@ class RestaurantsTestCase(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response_data['total'], 2)
         self.assertEqual(restaurant_2['id'], restaurant_1.id)
+
+    def test_api_key_validation(self):
+        # Este test valida el acceso al endpoint 'restaurants/' por medio de un API-KEY
+        response = self.client.get(reverse('list_restaurants'), formal='json')
+        response_data = json.loads(response.content)
+
+        self.assertEqual(response.status_code, 403)
 
 
 class ProductsTestCase(TestCase):
@@ -67,18 +74,31 @@ class ProductsTestCase(TestCase):
         for prodcut_data in response_data['products']:
             total_products_price_data += float(prodcut_data['price'])
 
-        self.assertEqual(response.status_code, 200)  # Valida codigo de estado        
-        self.assertEqual(response_data['total'], 5) # Valida la cantidad de productos        
-        self.assertEqual(response_data['restaurant'] 
-                         ['name'], restaurant_test.name) # Valida la cantidad de productos        
-        self.assertEqual(total_products_price, total_products_price_data) # Valida las sumatorias totales
+        self.assertEqual(response.status_code, 200)  # Valida codigo de estado
+        # Valida la cantidad de productos
+        self.assertEqual(response_data['total'], 5)
+        self.assertEqual(response_data['restaurant']
+                         ['name'], restaurant_test.name)  # Valida la cantidad de productos
+        # Valida las sumatorias totales
+        self.assertEqual(total_products_price, total_products_price_data)
+
+    def test_api_key_validation(self):
+        # Este test valida el acceso al endpoint 'restaurant/<int:pk>/products' por medio de un API-KEY
+        restaurant_test = Restaurant.objects.get(name='Siempre lleno')
+        products = Product.objects.filter(restaurant=restaurant_test.id)
+        response = self.client.get(reverse('list_products', kwargs={
+                                   'pk': restaurant_test.id}), formal='json')
+        response_data = json.loads(response.content)
+
+        self.assertEqual(response.status_code, 403)
 
 
 class ProductDetailTestCase(TestCase):
     # En esta clase serán ejecutadas las pruebas unitarias para endpoint que retorna el detalle de un producto
     def setUp(self):
         # La configuracion inicial crea un objeto Restaurant y agrega 3 productos
-        restaurant_test = Restaurant.objects.create(name='Arroz con Habichuela')
+        restaurant_test = Restaurant.objects.create(
+            name='Arroz con Habichuela')
         for i in range(0, 3):
             Product.objects.create(
                 name='Producto ' + str(i),
@@ -91,7 +111,8 @@ class ProductDetailTestCase(TestCase):
 
     def test_retrieve_product(self):
         restaurant_test = Restaurant.objects.get(name='Arroz con Habichuela')
-        product_test = Product.objects.filter(restaurant=restaurant_test.id).first()
+        product_test = Product.objects.filter(
+            restaurant=restaurant_test.id).first()
 
         # Realiza la peticion enviando el pk del primer producto creado
         response = self.client.get(reverse('product_detail', kwargs={
@@ -99,8 +120,23 @@ class ProductDetailTestCase(TestCase):
         self.assertEqual(response.status_code, 200)  # Valida codigo de estado
 
         response_data = json.loads(response.content)
-        self.assertEqual(product_test.name, response_data['product_detail']['name']) # Valida el nombre del producto retornado
-        self.assertEqual(product_test.id, response_data['product_detail']['id']) # Valida el ID del producto retornado
-        self.assertEqual(str(product_test.price), response_data['product_detail']['price']) # Valida el precio del producto retornado
+        
+        self.assertEqual(product_test.name,
+                         response_data['product_detail']['name']) # Valida el nombre del producto retornado
+        
+        self.assertEqual(
+            product_test.id, response_data['product_detail']['id']) # Valida el ID del producto retornado
+        
+        self.assertEqual(str(product_test.price),
+                         response_data['product_detail']['price']) # Valida el precio del producto retornado
 
+    def test_api_key_validation(self):
+        # Este test valida el acceso al endpoint 'products/<int:pk>' por medio de un API-KEY
+        restaurant_test = Restaurant.objects.get(name='Arroz con Habichuela')
+        product_test = Product.objects.filter(
+            restaurant=restaurant_test.id).first()
+        response = self.client.get(reverse('product_detail', kwargs={
+                                   'pk': product_test.id}), formal='json')
+        response_data = json.loads(response.content)
 
+        self.assertEqual(response.status_code, 403)
