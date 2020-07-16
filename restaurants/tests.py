ae17d3280ab228a0644a91f71dca262866042670
jcardenasc93 <jcardenasc93@gmail.com>
2020-07-16 03:37:51 -0500

    Test Case que valida la existencia del endpoint 'product/<int:pk>' y espera una respuesta con codigo de estado 200 (Falla)

--- restaurants/tests.py
+++ restaurants/tests.py
@@ -7,6 +7,7 @@ from .models import Restaurant, Product
 
 
 class RestaurantsTestCase(TestCase):
+    # En esta clase serán ejecutadas las pruebas unitarias para endpoint que retorna el listado de restaurantes
     def setUp(self):
         # La configuracion inicial crea 2 restaurantes
 
@@ -34,6 +35,7 @@ class RestaurantsTestCase(TestCase):
 
 
 class ProductsTestCase(TestCase):
+    # En esta clase serán ejecutadas las pruebas unitarias para endpoint que retorna el listado de productos asociados a un restaurante
     def setUp(self):
         # La configuracion inicial crea un objeto Restaurant y agrega 5 productos
         restaurant_test = Restaurant.objects.create(name='Siempre lleno')
@@ -51,7 +53,7 @@ class ProductsTestCase(TestCase):
         restaurant_test = Restaurant.objects.get(name='Siempre lleno')
         products = Product.objects.filter(restaurant=restaurant_test.id)
 
-        # Calcula la sumatoria de los precios de los productos creados 
+        # Calcula la sumatoria de los precios de los productos creados
         total_products_price = 0
         for prodcut in products:
             total_products_price += prodcut.price
@@ -59,14 +61,36 @@ class ProductsTestCase(TestCase):
         response = self.client.get(reverse('list_products', kwargs={
                                    'pk': restaurant_test.id}), formal='json')
         response_data = json.loads(response.content)
-        
+
         # Calcula la sumatoria de los precios de los productos consultados
         total_products_price_data = 0
         for prodcut_data in response_data['products']:
             total_products_price_data += float(prodcut_data['price'])
-            
 
-        self.assertEqual(response.status_code, 200)  # Valida codigo de estado     
-        self.assertEqual(response_data['total'], 5) # Valida la cantidad de productos
-        self.assertEqual(response_data['restaurant']['name'], restaurant_test.name) # Valida la cantidad de productos
+        self.assertEqual(response.status_code, 200)  # Valida codigo de estado        
+        self.assertEqual(response_data['total'], 5) # Valida la cantidad de productos        
+        self.assertEqual(response_data['restaurant'] 
+                         ['name'], restaurant_test.name) # Valida la cantidad de productos        
         self.assertEqual(total_products_price, total_products_price_data) # Valida las sumatorias totales
+
+
+class ProductDetailTestCase(TestCase):
+    # En esta clase serán ejecutadas las pruebas unitarias para endpoint que retorna el detalle de un producto
+    def setUp(self):
+        # La configuracion inicial crea un objeto Restaurant y agrega 3 productos
+        restaurant_test = Restaurant.objects.create(name='Arroz con Habichuela')
+        for i in range(0, 3):
+            Product.objects.create(
+                name='Producto ' + str(i),
+                description='''A continuación se describen las características del producto ofertado.
+                Se debe describir su sabor, sus ingredientes de forma tal que impacten el paladar del consumidor.
+                ''',
+                price=5000 * 12.0 / (i + 1),
+                restaurant=restaurant_test
+            )
+
+    def test_retrieve_product(self):
+        restaurant_test = Restaurant.objects.get(name='Arroz con Habichuela')
+        response = self.client.get(reverse('product_detail', kwargs={
+                                   'pk': restaurant_test.id}), formal='json')
+        self.assertEqual(response.status_code, 200)  # Valida codigo de estado
