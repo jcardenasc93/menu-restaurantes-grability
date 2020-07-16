b0cb51b75818b7bf6be18d9aa7bae94d2c30403e
jcardenasc93 <jcardenasc93@gmail.com>
2020-07-16 01:04:09 -0500

    Test case que valida que la longitud de elementos que retorna el endpoint 'restaurants/' concuerde con la cantidad de restaurantes creados y compara el id de un restaurante creado con uno de los retornados por el API (Falla)

--- restaurants/tests.py
+++ restaurants/tests.py
@@ -14,10 +14,20 @@ class RestaurantsTestCase(TestCase):
         restaurant_2 = Restaurant.objects.create(name='Restaurante Italiano')
 
     def test_ListRestaurants(self):
-        # Este test valida que el endpoint '/restaurants/' exista y que devuelva
-        # la cantidad correcta de restaurantes creados
+        """Este test valida que el endpoint '/restaurants/' exista y que devuelva
+        la cantidad correcta de restaurantes creados. Adicionalmente se valida
+        el nombre de un restaurante
+        """
 
         response = self.client.get(reverse('list_restaurants'), formal='json')
         response_data = json.loads(response.content)
+        restaurant_1 = Restaurant.objects.get(name='Coma y vuelva')
+
+        for resturant in response_data['restaurants']:
+            # Busca el restaurante que coincida con el nombre definido
+            if resturant['name'] == 'Coma y vuelva':
+                restaurant_2 = resturant
+
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(len(response_data), 2)
+        self.assertEqual(response_data['total'], 2)
+        self.assertEqual(restaurant_2['id'], restaurant_1.id)
