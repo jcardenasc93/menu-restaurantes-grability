9f12eb35d3d3427f3d2682e5a8a42964eb5bb085
jcardenasc93 <jcardenasc93@gmail.com>
2020-07-16 00:45:00 -0500

    Test case que valida que la longitud de elementos que retorna el endpoint 'restaurants/' concuerde con la cantidad de restaurantes creados (Falla)

--- restaurants/tests.py
+++ restaurants/tests.py
@@ -5,12 +5,19 @@ import json
 from .models import Restaurant
 # Create your tests here.
 
+
 class RestaurantsTestCase(TestCase):
     def setUp(self):
+        # La configuracion inicial crea 2 restaurantes
+
         restaurant_1 = Restaurant.objects.create(name='Coma y vuelva')
         restaurant_2 = Restaurant.objects.create(name='Restaurante Italiano')
-    
-    def test_ListRestaurants(self):
-        response = self.client.get(reverse('list_restaurants'), formal='json')
-        self.assertEqual(response.status_code, 200)
 
+    def test_ListRestaurants(self):
+        # Este test valida que el endpoint '/restaurants/' exista y que devuelva
+        # la cantidad correcta de restaurantes creados
+
+        response = self.client.get(reverse('list_restaurants'), formal='json')
+        response_data = json.loads(response.content)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response_data), 2)
