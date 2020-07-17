f363c5048c7cb79cf1838f5f02cdfca2cb073ecd
jcardenasc93 <jcardenasc93@gmail.com>
2020-07-16 23:43:33 -0500

    Elimina variable de entorno que no se requiere

--- restaurantMenus/settings.py
+++ restaurantMenus/settings.py
@@ -21,7 +21,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', None)
-API_SECRET_KEY = os.environ.get('API_SECRET_KEY', None)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', '') != 'False'
